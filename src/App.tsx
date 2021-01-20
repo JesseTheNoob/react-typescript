8c04e56d3cd6278d5e11c696a15a5c8eac3d959d
Jesse Louis Cuizon <cuizonjesse@gmail.com>
2021-01-20 20:54:27 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+
+class StudentInformation {
+  firstname: string = '';
+  lastname: string = '';
+  course: string = '';
+  yearLevel: number = 0;
+}
 
 function App() {
+  const [studentDataForm, setStudentDataForm] = useState<StudentInformation>(new StudentInformation());
+  const [studentData, setStudentData] = useState<StudentInformation[]>([]);
+  const addToForm = (): void => {
+    setStudentData([...studentData, {...studentDataForm}]);
+    setStudentDataForm(new StudentInformation());
+  }
+  const deleteItem = (index: number): void => {
+    setStudentData(studentData.filter((item, i) => index !== i))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>firstname</label>
+      <input type='text' value={studentDataForm.firstname} onChange={(e) => setStudentDataForm({...studentDataForm, firstname: e.target.value})} /> <br />
+      <label>lastname</label>
+      <input type='text' value={studentDataForm.lastname} onChange={(e) => setStudentDataForm({...studentDataForm, lastname: e.target.value})} /><br />
+      <label>course</label>
+      <input type='text' value={studentDataForm.course} onChange={(e) => setStudentDataForm({...studentDataForm, course: e.target.value})} /><br />
+      <label>year level</label>
+      <input type='number' value={studentDataForm.yearLevel} onChange={(e) => setStudentDataForm({...studentDataForm, yearLevel: Number(e.target.value)})} /><br />
+      <button onClick={addToForm}>add</button>
+      <hr />
+      <table>
+          <tr>
+            <th>
+              firstname
+            </th>
+            <th>
+              lastname
+            </th>
+            <th>
+              course
+            </th>
+            <th>
+              year level
+            </th>
+            <th>
+              actions
+            </th>
+          </tr>
+          {studentData.map((item, index) => (
+            <tr>
+              <td>
+                {item.firstname}
+              </td>
+              <td>
+                {item.lastname}
+              </td>
+              <td>
+                {item.course}
+              </td>
+              <td>
+                {item.yearLevel}
+              </td>
+              <td>
+                <button onClick={() => deleteItem(index)}>delete</button>
+              </td>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 }
